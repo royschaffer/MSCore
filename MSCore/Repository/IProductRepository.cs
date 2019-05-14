714860cc5aeaa43ae7da4fba670fee2d6141fd74
Roy <roy.schaffer@hotmail.com>
2019-05-14 01:14:41 -0300

    Creacion Proyecto

---
+++ MSCore/Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using MSCore.Models;
+using System.Collections.Generic;
+
+namespace MSCore.Repository
+{
+	public interface IProductRepository
+	{
+		void DeleteProduct(int productId);
+
+		Product GetProductByID(int productId);
+
+		IEnumerable<Product> GetProducts();
+
+		void InsertProduct(Product product);
+
+		void Save();
+
+		void UpdateProduct(Product product);
+	}
+}
